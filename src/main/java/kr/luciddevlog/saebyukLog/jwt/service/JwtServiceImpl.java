852ee2ddef893cec13eac39a39c92fcc24a6c5e6
dearmysolitude <lucid.dream07@proton.me>
2024-09-24 23:57:04 +0900

    [feat] jwt service onging 2

--- src/main/java/kr/luciddevlog/saebyukLog/jwt/service/JwtServiceImpl.java
+++ src/main/java/kr/luciddevlog/saebyukLog/jwt/service/JwtServiceImpl.java
@@ -1,15 +1,20 @@
 package kr.luciddevlog.saebyukLog.jwt.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.luciddevlog.saebyukLog.user.exception.UserNotFoundException;
 import kr.luciddevlog.saebyukLog.user.repository.UserItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
 
 public class JwtServiceImpl implements JwtService{
 
     // 환경 변수 주입
     @Value("${jwt.secretKey}")
-    private String secretKEy;
+    private String secretKey;
     @Value("${jwt.access.expiration")
     private Long accessTokenExpirationPeriod;
     @Value("${jwt.refresh.expiration}")
@@ -34,9 +39,42 @@ public class JwtServiceImpl implements JwtService{
         this.objectMapper = objectMapper;
     }
 
-//    @Override
-//    public String createAccessToken(String username) {
-//
-//    }
+    // 토큰 관리 로직
+    @Override
+    public String createAccessToken(String username) {
+        return JWT.create()
+                .withSubject(ACCESS_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(System.currentTimeMillis() + accessTokenExpirationPeriod))
+                .withClaim(USERNAME_CLAIM, username)
+                .sign(Algorithm.HMAC512(secretKey));
+    }
+
+    @Override
+    public String createRefreshToken() {
+        return JWT.create()
+                .withSubject(REFRESH_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(System.currentTimeMillis() + refreshTokenExpirationPeriod))
+                .sign(Algorithm.HMAC512(secretKey));
+    }
+
+    @Override
+    public void updateRefreshToken(String username, String refreshToken) {
+        userItemRepository.findByUsername(username)
+                .ifPresentOrElse(
+                        userItem -> userItem.updateRefreshToken(refreshToken),
+                        () -> new UserNotFoundException("존재하지 않는 회원")
+                );
+    }
+
+    @Override
+    public void destroyRefreshToken(String username) {
+        userItemRepository.findByUsername(username)
+                .ifPresentOrElse(
+                        userItem -> userItem.destroyRefreshToken(),
+                        () -> new UserNotFoundException("존재하지 않는 회원")
+                );
+    }
+
+    //
 
 }
--- src/main/java/kr/luciddevlog/saebyukLog/user/repository/UserItemRepository.java
+++ src/main/java/kr/luciddevlog/saebyukLog/user/repository/UserItemRepository.java
@@ -5,9 +5,11 @@ import kr.luciddevlog.saebyukLog.user.entity.UserItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserItemRepository extends JpaRepository<UserItem, Long> {
-    UserItem findByUsername(String username);
+    Optional<UserItem> findByUsername(String username);
     UserItem findByEmail(String phoneNumber);
     UserItem findByName(String name);
     UserItem findByRefreshToken(String refreshToken);
